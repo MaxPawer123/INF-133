773c5aebb52d2903c8ecf7bae8b791ea7a5d58b8
MaxPawer <gmamaniv@fcpn.edu.bo>
2024-02-28 09:50:14 -0400

    suma

--- soap-Client.py
+++ soap-Client.py
@@ -1,13 +1,12 @@
-from zeep import Client 
-result =Client.service.Saludar(nombre="Tatiana")
+from zeep import Client
+
+# Crear un cliente para el servicio web SOAP
+client = Client('http://ejemplo.com/servicio?wsdl')
+
+# Llamar a un método del servicio
+result = client.service.Saludar(nombre="Tatiana")
+
+# Hacer algo con el resultado
 print(result)
-
-def SumaDosNumeros(a,b):
-    return a+b
-
-
-#def CadenaPalindromo(a):
-
-
 
 
